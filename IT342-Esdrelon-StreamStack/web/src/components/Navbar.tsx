cd7d2f9032e5842f225be5fdb2d4cc033461a267
kaitlinesdrelon <kaitlin,nayde02@gmail.com>
2026-04-08 13:57:35 +0800

    feature/design-patterns-refactor

--- IT342-Esdrelon-StreamStack/web/src/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Navbar: React.FC = () => {
-  return <nav>Navbar Placeholder</nav>;
-};
-
-export default Navbar;
--- IT342-Esdrelon-StreamStack/web/src/types/User.tsx
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: string;
-  email: string;
-  name: string;
-}
-
-// If the file is still empty, just add this temporarily:
-export {};
